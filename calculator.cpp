4616ca15b8fbda019a6f6879e9afb4073a2b17bb
MAULIK AGARWAL <3picblackjack@gmail.com>
2023-07-03 22:25:49 +0530

    first commit

---
+++ calculator.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+using namespace std;
+
+int main() {
+  int op,a,b;
+  cout<<"enter the value of a and b and the operation :";
+   cin>>op>>a>>b;
+
+   if(op ==1)
+     cout<<a+b<<'\n';
+
+   else if(op ==2)
+      cout<<a-b<<'\n';
+
+   else if(op == 3)
+      cout<<a*b;
+
+   else if(op ==4)
+
+     cout<<"not in the mood to come in the nest week";
+
+  else 
+    cout<<"the vlue should be greater than the value is present value of the test case of ,more than is everyone offer the same test case pf doker ";
+return 0;}
